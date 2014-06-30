c11ad6c403d15b1da015b8c5f2e7e550df7ad8ba
Rico <teh_programerer@interactivelaboratories.com>
2014-07-01 00:48:22 +0100

    Initial commit, added a simple implementation of the level editor
    |INITIAL COMMIT|
    * Added GPLv3 [LICENSE] file
    * Added Git ignore rules
    * Added base source code, with a menu that exits the program, it starts
      maximised, and does nothing of note, yet

---
+++ Source/XNA/BBBEditor/OrthographicViewControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BBBEditor
+{
+	class OrthographicViewControl : GraphicsDeviceControl
+	{
+		protected override int Initialise( )
+		{
+			Application.Idle += delegate { Invalidate( ); };
+
+			m_ClearColour = new Color( 32, 96, 32 );
+
+			return 0;
+		}
+
+		protected override void Draw( )
+		{
+		}
+	}
+}
